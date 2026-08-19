01e3f1fcf971d3f8e52681ed942f2cac775241d8
dangToan <dangtoan.2k6@gmail.com>
2026-08-19 08:48:13 +0700

    fix: exclude DataSourceAutoConfiguration to fix startup error before db is configured

--- src/main/java/com/thuchanh/ThucHanhApplication.java
+++ src/main/java/com/thuchanh/ThucHanhApplication.java
@@ -3,7 +3,7 @@ package com.thuchanh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
 public class ThucHanhApplication {
 
     public static void main(String[] args) {
