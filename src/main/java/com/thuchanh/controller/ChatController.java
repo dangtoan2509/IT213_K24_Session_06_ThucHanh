ed10da8290989e34796e7295c740651a8b4236f4
dangToan <dangtoan.2k6@gmail.com>
2026-08-19 08:39:05 +0700

    fix: hardcode chat_memory_conversation_id to avoid AbstractChatMemoryAdvisor missing symbol

--- src/main/java/com/thuchanh/controller/ChatController.java
+++ src/main/java/com/thuchanh/controller/ChatController.java
@@ -3,7 +3,6 @@ package com.thuchanh.controller;
 import com.thuchanh.dto.ChatRequest;
 import com.thuchanh.dto.ChatResponse;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +22,7 @@ public class ChatController {
     public ChatResponse chat(@RequestBody ChatRequest request) {
         String response = this.chatClient.prompt()
                 .user(request.getUserMessage())
-                .advisors(a -> a.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID, request.getConversationId()))
+                .advisors(a -> a.param("chat_memory_conversation_id", request.getConversationId()))
                 .call()
                 .content();
 
