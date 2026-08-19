ae375cef2dc8e225181946dd6e19f31580cf4c2b
dangToan <dangtoan.2k6@gmail.com>
2026-08-19 08:51:42 +0700

    fix: use excludeName for DataSourceAutoConfiguration to avoid compilation error

--- src/main/java/com/thuchanh/ThucHanhApplication.java
+++ src/main/java/com/thuchanh/ThucHanhApplication.java
@@ -3,7 +3,7 @@ package com.thuchanh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
+@SpringBootApplication(excludeName = {"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"})
 public class ThucHanhApplication {
 
     public static void main(String[] args) {
